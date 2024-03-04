04e3e8395c5d2e6a266084f74591faa6245d895e
Nancylovesdata <nancyeofreeman@gmai.com>
2024-03-04 17:33:57 +0000

    fixed erors

--- src/components/aboutus.jsx
+++ src/components/aboutus.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 export default function Aboutus() {
   return (
     <div>
-      
       <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore sint possimus sapiente ad quis enim placeat rem esse iste ea, impedit amet accusamus voluptatibus, non quas officia quisquam praesentium ab?
         </p>
--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Aboutus } from '../components/aboutus'
+import  Aboutus  from '../components/aboutus'
 import Navbar from '../components/navbar'
 
-export const Aboutpage = () => {
+export default function Aboutpage()  {
   return (
     <div>
         <h1>This is my about page</h1>
     <Navbar/>
-    <Aboutpage/>
+    <Aboutus/>
     </div>
   )
 }
 
-export default Aboutpage;
+//  Aboutpage;
